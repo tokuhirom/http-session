63d3360d59201c41f6ea543e973d758a1467e754
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-19 02:54:22 +0000

    Checking in changes prior to tagging of version 0.20.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 32784)
    +++ Changes	(local)
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::Session
     
    +0.20
    +
    +        - no Moose; for less memory/less loadtime
    +
     0.11
     
             - fixed test problem(no feature changes)
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-Session/trunk@24219 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/Session.pm
+++ lib/HTTP/Session.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base qw/Class::Accessor::Fast/;
 use 5.00800;
-our $VERSION = '0.11';
+our $VERSION = '0.20';
 use Digest::SHA1 ();
 use Time::HiRes ();
 use Moose::Util::TypeConstraints;
