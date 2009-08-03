9059f2075d9acde9b0fe6e5b38a84bd669c31a0c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-08-03 10:53:51 +0900

    Checking in changes prior to tagging of version 0.32.  Changelog diff is:
    diff --git a/Changes b/Changes
    index e23fdc9..7d06740 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::Session
    
    +0.32
    +
    +    - fixed deps for URI.pm
    +
     0.31
    
         - State::URI: added / to input tag

--- lib/HTTP/Session.pm
+++ lib/HTTP/Session.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base qw/Class::Accessor::Fast/;
 use 5.00800;
-our $VERSION = '0.31';
+our $VERSION = '0.32';
 use Carp ();
 use Scalar::Util ();
 use UNIVERSAL::require;
