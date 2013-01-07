c244de3ed7af6b0490e8c1f0983aa6d5358eea04
tokuhirom <tokuhirom@gmail.com>
2013-01-07 14:09:43 +0900

    0.47

--- lib/HTTP/Session.pm
+++ lib/HTTP/Session.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base qw/Class::Accessor::Fast/;
 use 5.00800;
-our $VERSION = '0.46';
+our $VERSION = '0.47';
 use Carp ();
 use Scalar::Util ();
 use Module::Runtime ();
